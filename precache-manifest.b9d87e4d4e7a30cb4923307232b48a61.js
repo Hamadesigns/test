f46cd69fc6365e22dde10d234ddbe481a546489e
hamadesigns <hamadesigns@outlook.com>
2018-12-16 21:34:29 +0500

    Updates

---
+++ precache-manifest.b9d87e4d4e7a30cb4923307232b48a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "3c0e6695001b520ed312",
+    "url": "/test/static/js/main.3c0e6695.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/test/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "3c0e6695001b520ed312",
+    "url": "/test/static/css/main.d0cb4e04.chunk.css"
+  },
+  {
+    "revision": "e3caa518ae14395ad267c03a973d10d2",
+    "url": "/test/index.html"
+  }
+];
